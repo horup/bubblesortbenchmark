afa75c065d4c6aa0415bd63c5cb48ad5ea021126
horup <sah@sah.dk>
2020-06-11 12:17:53 +0200

    updated test

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace csharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void UsingLists()
         {
             int num = 50*1000;
             int rerun = 10;
@@ -29,8 +29,40 @@ namespace csharp
             }
 
             int took = Environment.TickCount - tick;
+            Console.WriteLine($"{took}ms to sort {array.Count} elements {rerun} times  (List)");
+        }
+        static void UsingArrays()
+        {
+            int num = 50*1000;
+            int rerun = 10;
+            int[] array = new int[num];
+            for (var i = 0; i < num; i++)
+                array[i] = array.Length;
 
-            Console.WriteLine($"{took}ms to sort {array.Count} elements {rerun} times");
+            int tick = Environment.TickCount;
+
+            for (int r = 0; r < rerun; r++)
+            {
+                for (int i = 0; i < array.Length; i++)
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        if (array[i] < array[j])
+                        {
+                            int org = array[i];
+                            array[i] = array[j];
+                            array[j] = org;
+                        }
+                    }
+            }
+
+            int took = Environment.TickCount - tick;
+            Console.WriteLine($"{took}ms to sort {array.Length} elements {rerun} times (Array)");
+        }
+
+        static void Main(string[] args)
+        {
+            UsingLists();
+            UsingArrays();
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,13 +1,15 @@
 
+
 C:\Users\soh\Desktop\PROJECTS\jsvscsharp>dotnet run --configuration=Release
-15094ms to sort 50000 elements 10 times
+28500ms to sort 50000 elements 10 times  (List)
+13782ms to sort 50000 elements 10 times (Array)
 
 C:\Users\soh\Desktop\PROJECTS\jsvscsharp>node index.js
-28326ms to sort 50000 elements 10 times
+27613ms to sort 50000 elements 10 times
 
 Conclusion:
 Dotnet and node is similar in performance when using List.
-Dotnet a factor 1.87 faster than node for bubblesort when using array in dotnet
+Dotnet a factor 1.87 faster than node for bubblesort when using array in dotnet.
 
 Todo:
 - Check if it is possible to run node in "Release" configuration
