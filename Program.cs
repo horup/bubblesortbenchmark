7762755eaf2aa9e6511232cb9af62d399533bd72
horup <sah@sah.dk>
2020-06-11 08:06:45 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 50*1000;
+            int retry = 10;
+            int[] array = new int[num];
+            for (var i = 0; i < num; i++)
+                array[i] = array.Length - i;
+
+            int tick = Environment.TickCount;
+
+            for (int i = 0; i < array.Length; i++)
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        int org = array[i];
+                        array[i] = array[j];
+                        array[j] = org;
+                    }
+                }
+
+            int took = Environment.TickCount - tick;
+
+            Console.WriteLine($"{took}ms to sort {array.Length} elements");
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+let num = 50*1000;
+let array = [];
+for (var i = 0; i < num; i++)
+    array[i] = array.length - i;
+
+let tick = new Date().getTime();
+
+for (let i = 0; i < array.length; i++)
+    for (let j = 0; j < array.length; j++)
+    {
+        if (array[i] < array[j])
+        {
+            let org = array[i];
+            array[i] = array[j];
+            array[j] = org;
+        }
+    }
+
+let took = new Date().getTime() - tick;
+
+console.log(`${took}ms to sort ${array.length} elements`);
