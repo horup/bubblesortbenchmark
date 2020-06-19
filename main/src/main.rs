db0e71502c61f8665deb8e64d749848b558132a0
Christian Leicht <cle@skov.com>
2020-06-19 10:58:41 +0200

    added rust bubble sort implementation.

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,36 @@
+use std::time::Instant;
+ 
+fn bubble_sort_optimized(arr: &mut [i16]) {
+     let mut new_len: usize;
+    let mut len = arr.len();
+    loop {
+        new_len = 0;
+        for i in 1..len {
+            if arr[i - 1] > arr[i] {
+                arr.swap(i - 1, i);
+                new_len = i;
+            }
+        }
+        if new_len == 0 {
+            break;
+        }
+        len = new_len;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+	let mut array = [0_i16; 50 * 1000];
+	let len = array.len();
+	for i in 1..len {
+		array[i] = i as i16;
+	}
+	
+	let rerun = 10;
+	let start = Instant::now();
+	for _q in 1..rerun {
+		bubble_sort_optimized(&mut array);
+	}
+	let duration = start.elapsed();
+	
+    println!("Took: {:?} to sort {:?} elements {:?} times", duration, array.len(), rerun); 
 }
