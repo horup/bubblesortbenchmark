182280e86911469853ee6e9dde8e2f2f5c08ca8b
horup <sah@sah.dk>
2020-06-22 10:55:35 +0200

    refactored and added rust impl to start.bat

--- start.bat
+++ start.bat
@@ -2,6 +2,8 @@ clang -O -fsanitize=address main.cpp && a.exe
 clang -O main.cpp && a.exe
 dotnet run --configuration=Release
 node index.js
+rustc -O main.rs -o rust.exe
+./rust.exe
 
 cd main
 cargo run
